f78d1114c4ff04eb7200405f44554eed3eaa45fe
Standalone Developer <dev@example.com>
2026-08-09 11:08:30 +0530

    changes-logos-app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.threewin.tictactoe"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.2"
+        versionCode = 3
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
